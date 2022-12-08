81d36b15409e3c21f0650e573f87f13da08d55c6
Đurđa Mićić <micicdjurdja@gmail.com>
2022-12-08 18:36:08 +0100

    updating

--- week6/JS/exercise_functions5A.js
+++ week6/JS/exercise_functions5A.js
@@ -2,21 +2,60 @@
 // ispisuje na stranici po jedan citat, ali tako da se citat promeni kada se program
 // pokrene.
 
+
+
 // 2. Napisati funkciju koja za godinu (kao ceo pozitivan broj) proverava da li je
 // prestupna. Godina je prestupna ako je deljiva sa 4 i nije godina veka (znači ako
 // nije deljiva sa 100 - 1900. nije bila prestupna). Od toga odstupa svaka četvrta
 // godina veka (znači ako je deljiva sa 400, onda je ipak prestupna - npr. 2000. je
 // prestupna).
 
+
+
 // 3. Napisati funkciju koja za zadatu reč (ili rečenicu) proverava koliko ima
 // samoglasnika i koliko suglasnika.
+
+// function vowelsConsonatns(a){
+//     const b = a.split('')
+//     var counterV = 0;
+//     var counterC = 0;
+//     for(let i = 0; i < b.length; i++){
+//         if(b[i] === 'a' || b[i] === 'e' || b[i] === 'i' || b[i] === 'o' || b[i] === 'u'){
+//             counterV++
+//         } else counterC ++
+//     } return ['consonants: ' + counterC,' and vowels: ' + counterV];
+// }
+// console.log(vowelsConsonatns('lets check with this aaa'));
 
 // 4. Kreirati funkciju koja prihvata array, a na izlazu ispisuje niz koji sadrži tipove
 // podataka pojedinačnih elemenata ulaznog niza.
 
-// 5. Kreirati funkciju koja prijvata niz stringova kao ulayni parametar, a na izlazu
+    // function typeOf(a){
+    //     const b = [];
+    //     for(let i = 0; i < a.length; i++){
+    //         b[i] = typeof a[i]
+    //     } return b 
+    // }
+    // console.log(typeOf(['hello', 8392, NaN, Infinity, undefined, 'foew', 23, 2e2]));
+
+
+// 5. Kreirati funkciju koja prijvata niz stringova kao ulazni parametar, a na izlazu
 // ispisuje samo stringove koji u sebi ne sadrže brojeve. Primer: ulazni niz [“12bb”,
-// “pp”, “as23s”, “00sd”] =&gt; rezultat [“pp”].
+// “pp”, “as23s”, “00sd”] => rezultat [“pp”].
+
+
+    // function stringCheck(a){
+    //     const b = [];
+    //     var afterSplit = '';
+    //         for(let i = 0; i < a.length; i++){
+    //             afterSplit = a[i].split('')
+    //         if (isNaN(afterSplit[i]) == true){
+    //             b.push(a[i]) 
+    //         }
+    //     } return b
+    // }
+    // console.log(stringCheck(['12bb','pp', 'as23s', 'fjoew3', 'fowe']));
+
 
 // 6. Kreirati funkciju koja prima string a kao rezultat ispisuje niz koji u sebi ima:
 // dužinu stringa, prvi karakter stringa, poslednji karakter, središnji karakter ako
@@ -24,8 +63,8 @@
 // broj znakova, indeks drugog ponavljanja drugog karaktera u formatu “@ Index # ”
 // ili “not found” ako nema ponavljanja.
 
-// Primer 1: &quot;Computer&quot; =&gt; [8, &quot;C&quot;, &quot;r&quot;, &quot;pu&quot;, &quot;not found&quot;] ,
-// Primer 2: “Science&quot; =&gt; [7, &quot;S&quot;, &quot;e&quot;, &quot;e&quot;, &quot;@ index 5&quot;]
+// Primer 1: 'Computer' => [8, 'C', 'r', 'pu', 'not found'] ,
+// Primer 2: 'Science' => [7, 'S', 'e', 'e', '@ index 5']
 
 // 7. Dat je ulazni niz [9, 5, 6, 8, 7, 7, 1, 1, 1, 1, 1, 9, 8] gde se svaki element ponavlja
 // bar jednom osim dva elementa. Kreirati funkciju koja ispisuje dva elementa koja
@@ -34,3 +73,14 @@
 // 8. Kreirati funkciju koja ulazni niz deli u podnizove duzine N i ispisuje ih.
 // Primer 1: ([2, 3, 4, 5], N=2) => [[2, 3], [4, 5]]
 // Primer 2: ([2, 3, 4, 5, 6], N=3) => [[2, 3, 4], [5, 6]]
+
+    function createSubArrays(a, n){
+        const b = [];
+        for(let i = 0; i < a.length; i++){
+            if(n > 0){
+                b.push(a[i]);
+                b = b.length(n)
+            }
+        } return b
+    }
+    console.log(createSubArrays([2, 3, 4, 5], 2))
--- week6/JS/exercise_functions_predetermined.js
+++ week6/JS/exercise_functions_predetermined.js
@@ -1,21 +1,68 @@
 // 1. Write a function that converts an array of strings into an array of numbers. Filter
 // out all non-numeric values.
-// Input: [&quot;1&quot;, &quot;21&quot;, undefined, &quot;42&quot;, &quot;1e+3&quot;, Infinity]
+// Input: ['1', '21', undefined, '42', '1e+3', Infinity]
 // Output: [1, 21, 42, 1000]
+
+function onlyNumbers(a){
+    var result = [];
+    for (var i = 0; i < a.length; i++) {
+        if (isFinite(a[i])) {
+          result.push(+a[i]);
+        }
+      }
+      return result;
+} 
+console.log(onlyNumbers(['1', '21', undefined, '42', '1e+3', Infinity]))
 
 // 2. Write a program to join all elements of the array into a string skipping elements
 // that are undefined, null, NaN or Infinity.
 // Input: [NaN, 0, 15, false, -22, &#39;&#39;, undefined, 47, null]
 // Output: “015false-2247”
 
+function joinIntoString(a) {
+    var result = a.filter(function (b) {
+      return b !== undefined && b !== null && b !== Infinity && !isNaN(b);
+    });
+    return result.join('')
+  }
+  console.log(joinIntoString([NaN, 0, 15, false, -22, "", undefined, 47, null]));
+
 // 3. Write a program to filter out falsy values from the array.
 // Input: [NaN, 0, 15, false, -22, &#39;&#39;, undefined, 47, null]
 // Output: [15, -22, 47]
 
+function filterFalsy(a) {
+    var result = a.filter(Boolean);
+    return result
+}
+console.log(filterFalsy([NaN, 0, 15, false, -22, '', undefined, 47, null]))
+
 // 4. Write a program that calculates a number of integer values in the array.
-// Input: [NaN, 23.1, 15, false, -22.5, &#39;&#39;, 4, 7, null]
+// Input: [NaN, 23.1, 15, false, -22.5, '', 4, 7, null]
 // Output: 3
 
+function howManyIntegers(a) {
+    var counter = 0;
+    for (var i = 0; i < a.length; i++) {
+      if (Number.isInteger(a[i])) {
+        counter++;
+      }
+    }
+    return counter;
+  }
+  console.log(howManyIntegers([NaN, 23.1, 15, false, -22.5, "", 4, 7, 8, null]))
+
 // 5. Write a program that calculates a number of float values in the array.
-// Input: [NaN, 23.1, 15, false, -22.5, &#39;&#39;, 4, 7, null]
+// Input: [NaN, 23.1, 15, false, -22.5, '', 4, 7, null]
 // Output: 2
+
+function howManyFloat(a) {
+    var counter = 0;
+    for (var i = 0; i < a.length; i++) {
+      if (typeof a[i] === "number" && !Number.isNaN(a[i]) && !Number.isInteger(a[i])) {
+        counter++;
+      }
+    }
+    return counter
+  }
+  console.log(howManyFloat([NaN, 23.1, 15, false, -22.5, 9.8, "", 4, 7, null]))
